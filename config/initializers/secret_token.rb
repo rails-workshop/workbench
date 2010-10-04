9f5a50a9561572a8f2d93d39c3b2f3900a99fcdc
Mikhail Vaysman <wiseman@wiseman-nb.(none)>
2010-10-05 03:29:38 +0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workbench::Application.config.secret_token = 'be91251b9968062fe4cdcd104bcf5943341cd9c7e5146c3a5edaa2aa6505b1ea020dca79571f75c583beed2ff7796c95496711eca5960f602c8b6070a237e44a'
